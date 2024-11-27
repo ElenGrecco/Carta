783994030d99002d934ec504e94607037e0d4703
Elen Grecco <elen_grecco@hotmail.com>
2024-11-26 21:10:27 -0300

    Atualização

---
+++ app.js
@@ -0,0 +1,20 @@
+/**
+ * Sorteio de uma carta
+ * Exemplo de uso de array de um vetor
+ * @author Elen Grecco
+ */
+
+function sorteio() {
+    let nipes = ['♥', '♦', '♣', '♠']
+    let faces = ["A", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+
+//sorteio do índice do vetor
+let nipeSorteada = nipes [Math.floor(Math.random() * 4)] // 0 1 2 3
+let faceSorteada = faces [Math.floor(Math.random() * 13)] // 0 1 2 3 4 5 6 7 8 9 10 11 12
+
+   // console.log(`${faceSorteada} de ${nipeSorteada}`)
+}
+
+
+
+
